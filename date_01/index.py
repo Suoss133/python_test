31094965146cd2865ab1a9e3c3c181d9d9aeb5e6
lipenghui <liph@dazhuanjia.com>
2020-11-13 22:14:38 +0800

    upata list sort function

--- date_01/index.py
+++ date_01/index.py
@@ -1,1 +1,28 @@
-print('Hello World')
+array = [2, 8, 1, 5]
+# arr1 = array[0]
+# arr2 = array[1]
+# array[0] = arr2
+# array[1] = arr1
+# print(array)
+
+
+lists = []
+for i in range(len(array)):
+    min_num = array.index(min(array))
+    lists1 = array.pop(min_num)
+    lists.append(lists1)
+print(lists)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
