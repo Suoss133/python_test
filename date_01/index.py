20394f55fccd34b56c414f23783d35f2f526fb29
lipenghui <liph@dazhuanjia.com>
2020-11-19 21:50:35 +0800

    添加九九乘法表

--- date_01/index.py
+++ date_01/index.py
@@ -6,23 +6,19 @@ array = [2, 8, 1, 5]
 # print(array)
 
 
-lists = []
-for i in range(len(array)):
-    min_num = array.index(min(array))
-    lists1 = array.pop(min_num)
-    lists.append(lists1)
+# lists = []
+# for i in range(len(array)):
+#     min_num = array.index(min(array))
+#     lists1 = array.pop(min_num)
+#     lists.append(lists1)
+# print(lists)
+
+# for i in range(1,10):
+#     for j in range(1,10):
+#         if i >= j:
+#             print(f'{j}*{i}={i * j}', end=' ')
+#     print('\n')
+
+lists = [(str(j) + '*' + str(i) + '=' + str(i * j)) for i in range(1, 10)
+                                                    for j in range(1, i + 1)]
 print(lists)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
